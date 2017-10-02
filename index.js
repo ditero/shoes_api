5c2be748e4b306e62d6603d3f57d6dd5dec52320
ditero <anelesigenu@gmail.com>
2017-10-02 16:57:39 +0200

    Updating index.js for API Access Control

--- index.js
+++ index.js
@@ -18,15 +18,15 @@ app.set('view engine', 'hbs')
 app.use('/', express.static('public'))
 
 // Catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error("Not Found");
+//   err.status = 404;
+//   next(err);
+// });
 
 // Error handler
 app.use(function(err, req, res, next) {
-  res.status(err, status || 500)
+  res.status(err.status || 500)
   res.json({
     error:{
       message: err.message
