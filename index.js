d9508a889251d1e2a73b8a312c311325d68cc842
ditero <anelesigenu@gmail.com>
2017-10-02 16:19:54 +0200

    Updating index.js for API Access Control

--- index.js
+++ index.js
@@ -17,9 +17,31 @@ app.engine('hbs', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'hbs')
 app.use('/', express.static('public'))
 
-// app.use(function(req, res) {
-//   res.status(404).send({url: req.originalUrl + ' not found'})
-// });
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+  res.status(err, status || 500)
+  res.json({
+    error:{
+      message: err.message
+    }
+  })
+})
+app.use(function(req, res, next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	if(req.method === "OPTIONS") {
+		res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE,GET");
+		return res.status(200).json({});
+	}
+	next();
+});
 //prase application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
 
@@ -29,6 +51,8 @@ app.use(bodyParser.json())
 //Use the session middleware
 app.use(session({secret: 'keyboard cat', cookie:{maxAge: 60000 * 20}}))
 app.use(flash())
+
+
 
 //Shoes api routes
 app.get('/api/shoes', shoesRoutes.shoesInStock)
