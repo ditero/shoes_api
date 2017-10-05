84be6e4dcbc841c67fb1debc371474ae6cc8edca
ditero <anelesigenu@gmail.com>
2017-10-05 16:23:04 +0200

    Setting API for online environment

--- public/js/queryBrandAndSize.js
+++ public/js/queryBrandAndSize.js
@@ -1,7 +1,7 @@
 // Search the given size and brand using AJAX Call
 function searchBrandAndSize(myshoes, brand, size, shoesTemplate){
   $.ajax({
-    url: "https://shoe-cart-api.herokuapp.com/api/shoes/"+brand+"/size/"+size, //"http://localhost:8000/api/shoes/brand/"+brand+"/size/"+size ||
+    url: "https://shoe-cart-api.herokuapp.com/api/shoes/brand/"+brand+"/size/"+size, //"http://localhost:8000/api/shoes/brand/"+brand+"/size/"+size ||
     type: "GET"
   }).done(function(shoesData){
     myShoes.innerHTML = shoesTemplate({
