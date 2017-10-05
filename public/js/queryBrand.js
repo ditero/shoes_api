9f8969d0f53b76e505f92fd39d8c951588cc9931
ditero <anelesigenu@gmail.com>
2017-10-05 16:32:59 +0200

    Setting API for online environment

--- public/js/queryBrand.js
+++ public/js/queryBrand.js
@@ -1,7 +1,7 @@
 // Search the given brand using AJAX Call
 function searchBrand(myshoes, brand, shoesTemplate){
   $.ajax({
-    url: "https://shoe-cart-api.herokuapp.com/api/shoes/brand/"+brand, //"http://localhost:8000/api/shoes/brand/"+brand || 
+    url: "https://shoe-cart-api.herokuapp.com/api/shoes/brand/"+brand, //"http://localhost:8000/api/shoes/brand/"+brand ||
     type: "GET"
   }).done(function(shoesData){
     myShoes.innerHTML = shoesTemplate({
@@ -16,7 +16,7 @@ $(".selectBrand").on('change', function(e){
 thisBrand = this.children.brand.value
 
 //Conditional statement to check which drop option has been selected
-  if (thisBrand !== "Brands" && (thisSize !== "Sizes" || thisSize === undefined)) {
+  if (thisBrand !== "Brands" && (thisSize || thisSize === "Sizes" || thisSize === undefined)) {
     searchBrand(myShoes, thisBrand, shoesTemplate)
   }else if (thisBrand !== "Brands" && thisSize !== "Sizes") {
     searchBrandAndSize(myShoes, thisBrand, thisSize, shoesTemplate)
